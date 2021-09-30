0a12261ceeb9392397b0b5d164c96537b5b556db
swartz007 <64153285+swartz007@users.noreply.github.com>
2021-09-30 18:07:09 +0530

    Quick Update

---
+++ includes/common.php
@@ -0,0 +1,3 @@
+<?php
+$con = mysqli_connect("localhost:3307","root","","bank_db1") or die(mysqli_error($con));
+?>
---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-fixed-top " style="background-color:#85929E ;">
+<div class="container">
+<div class="navbar-header">
+<button class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" style="color: #000000; font-size:30px;">Sparks Foundation Bank</a>
+</div>
+<div class="collapse navbar-collapse" id="mynavbar">
+<ul class="nav navbar-nav navbar-right" >
+<li><a href = "index.php" style="color: #000000; font-size:20px"><span class = "glyphicon glyphicon-home"></span> Home</a></li>
+<li><a href="user.php" style="color: #000000; font-size:20px"><span class="glyphicon glyphicon-user"></span> New Users</a></li>
+<li><a href="transaction.php" style="color: #000000; font-size:20px"><span class="glyphicon glyphicon-transfer"></span> Transfer Money</a></li>
+<li><a href="history.php" style="color: #000000; font-size:20px"><span class="glyphicon glyphicon-inbox"></span> History</a></li>
+
+</ul>
+</div>
+</div>
+</nav>
